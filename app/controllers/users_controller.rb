32bc5d4b37cd66ec8e1e4baa09e0984cd45c50c7
shrijamadhu <shrija@giet.ac.in>
2021-09-27 22:31:03 +0530

    added index,show,login,create for users in user_controller.rb

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,28 @@
+class UsersController < ApplicationController
+  skip_before_action :verify_authenticity_token
+  def index
+    render plain: User.all.map{|user| user.to_pleasant_string}.join("\n")
+  end
+  def show
+    id = params[:id]
+    user = User.find(id)
+    render plain: user.to_pleasant_string
+  end
+  def create
+    name = params[:name]
+    email = params[:email]
+    password = params[:password]
+    new_user = User.create!(name: name,email: email,password: password)
+    render plain: "hey! created new user with id #{new_user.id}"
+  end
+  def login
+    email = params[:email]
+    password = params[:password]
+    new_user = User.find_by(email: email,password: password)
+    if !new_user
+      render plain: "false"
+    else
+      render plain: "true"
+    end
+  end
+end
