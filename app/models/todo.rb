61c81bca09d0b951e725c7d8045652a565fa1f15
shrijamadhu <shrija@giet.ac.in>
2021-09-27 10:31:48 +0530

    Single todo search enabled

--- app/models/todo.rb
+++ app/models/todo.rb
@@ -1,2 +1,8 @@
 class Todo < ActiveRecord::Base
+  def to_pleasant_string
+    is_complete = completed ? "[X]" : "[ ]"
+    "#{id}. #{todo_text}     #{due_date} #{is_complete} "
+  end
+
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   get "todos", to: "todos#index"
+  get "todos/:id", to: "todos#show"
 
 end
