6a64e2b971fe49fa622721f3ee6eee4ba66cce19
shrijamadhu <shrija@giet.ac.in>
2021-09-27 22:27:17 +0530

    added resources for users in routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,6 @@ Rails.application.routes.draw do
   # post "todos/create",to: "todos#create"
   # get "todos/:id", to: "todos#show"
   resources :todos
+  resources :users
+  post "users/login",to: "users#login"
 end
